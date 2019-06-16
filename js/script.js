063725f8739c32208be5d2778de655074b551698
Pavel Vlasov <pavelvlasovwork@gmail.com>
2019-06-16 17:58:25 +0300

    ошибка

--- js/script.js
+++ js/script.js
@@ -10,23 +10,66 @@ function modalClose() {
   }
 }
 
-var contactsLink = document.querySelector('.contacts-button');
+var contactsLink = document.querySelector('.write-us-button');
 var modalWriteUs = document.querySelector('.modal-write-us');
 
 
 if (modalWriteUs) {
   var modalWriteUsClose = modalWriteUs.querySelector('.modal-close');
 
+  var contactsForm = modalWriteUs.querySelector('.write-us-form');
+  var userData = {
+    name: document.getElementById('write-us-name'),
+    email: document.getElementById('write-us-email'),
+    message: document.getElementById('write-us-message')
+  };
+
+  var isStorageSupport = true;
+  var storage = {
+    name: '',
+    email: ''
+  };
+
   contactsLink.addEventListener('click', function (evt) {
     evt.preventDefault();
     modalShow(modalWriteUs);
+
+    if (storage.name && storage.email) {
+      userData.name.value = storage.name;
+      userData.email.value = storage.email;
+      userData.message.focus();
+    } else {
+      userData.name.focus();
+    }
   });
 
   modalWriteUsClose.addEventListener('click', function (evt) {
     evt.preventDefault();
     modalClose();
   });
+
+  contactsForm.addEventListener('submit', function (evt) {
+    if (!userData.name.value || !userData.email.value || !userData.message.value) {
+      evt.preventDefault();
+      modalWriteUs.classList.remove('modal-error');
+      void modalWriteUs.offsetWidth;
+      modalWriteUs.classList.add('modal-error');
+
+      if (!userData.name.value) {
+        userData.name.focus();
+      } else if (!userData.email.value) {
+        userData.email.focus();
+      } else {
+        userData.message.focus();
+      }
+    } else if (isStorageSupport) {
+      localStorage.setItem('name', userData.name.value);
+      localStorage.setItem('email', userData.email.value);
+    }
+  });
 }
+
+
 
 
 var mapLink = document.querySelector('.map-image');
